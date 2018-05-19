7d10b23f5acd19306a687e79ae45e91ae0488469
Sam Coe <coe6@purdue.edu>
2018-05-18 23:39:07 -0400

    Create app and add toolbar to homescreen

---
+++ Pic! a Puppy (GUS VERSION)/Pic! a Puppy (GUS VERSION)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pic! a Puppy (GUS VERSION)
+//
+//  Created by Sam on 5/18/18.
+//  Copyright © 2018 github.com/coe6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
